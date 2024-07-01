541c19bd14df71b0b5c277aa4f886c3349ee7fa1
rlee17 <rachel_lee@intuit.com>
2024-06-30 23:06:46 -0700

    create xcode project

---
+++ Notepad/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Notepad
+//
+//  Created by Rachel Lee on 6/30/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
